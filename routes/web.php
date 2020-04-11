e7b18e941a49670e413253b52b5fdcff9980bc16
mutado <mutado.nzr@gmail.com>
2020-04-11 23:47:57 +0300

    Added redis broadcast

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/t', function () {
+    event(new \App\Events\MessageSent());
+    dd('Event Run Successfully.');
+});
